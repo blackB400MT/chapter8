554903b249fbb156c32556d55b6c620e34684718
root <root@localhost.localdomain>
2018-02-15 10:25:49 +0000

    Adding new modules

---
+++ modules/dirlister.py
@@ -0,0 +1,9 @@
+import os
+
+# 8.2 - 109
+
+def run(**args):
+	print "[*] in dirlister module."
+	files = os.listrid(".")
+	
+	return str(files)
---
+++ modules/environment.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os 
+
+def run(**args):
+    print "[*] In environment module."
+    return str(os.environ)
+
+
